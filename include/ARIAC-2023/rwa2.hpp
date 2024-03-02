6203c2b917377274d8721d9957578c016b189c48
Kiran S Patil <kiran.suvas.patil@gmail.com>
2024-03-02 16:34:50 -0500

    updated code for rwa 2

--- include/ARIAC-2023/rwa2.hpp
+++ include/ARIAC-2023/rwa2.hpp
@@ -12,7 +12,7 @@
 #include <chrono>
 #include <memory>
 #include <thread>
-
+#include <map>
 #include "rclcpp/rclcpp.hpp"
 #include "ariac_msgs/msg/competition_state.hpp"
 #include "std_srvs/srv/trigger.hpp"
@@ -24,6 +24,11 @@
 #include "ariac_msgs/msg/kitting_task.hpp"
 #include "ariac_msgs/msg/kitting_part.hpp"
 #include "ariac_msgs/msg/bin_parts.hpp"
+#include "ariac_msgs/msg/conveyor_parts.hpp"
+#include "ariac_msgs/msg/bin_info.hpp"
+#include "ariac_msgs/msg/part_lot.hpp"
+#include "ariac_msgs/msg/part.hpp"
+
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/vector3.hpp"
 
@@ -103,6 +108,79 @@ namespace order_
 
 }
 
+/**
+ * @brief Namespace defined to identify specific data structures that define part colors and types
+ * 
+ */
+namespace color_
+{   
+    /**
+     * @brief Class defined to store information about part colors and types. It uses a map to store that information
+     * 
+     */
+    class ColorParts{
+        
+        public:
+
+        std::map<int,std::string> COLOR={
+            {0,"RED"},
+            {1,"GREEN"},
+            {2,"BLUE"},
+            {3,"ORANGE"},
+            {4,"PURPLE"}};
+
+        std::map<int,std::string> PART={
+            {10,"BATTERY"},
+            {11,"PUMP"},
+            {12,"SENSOR"},
+            {13,"REGULATOR"}};
+
+        std::map<int,std::string> DEST={
+            {0,"KITTING"},
+            {1,"ASSEMBLY_FRONT"},
+            {2,"ASSEMBLY_BACK"},
+            {3,"WAREHOUSE"}};
+    };
+    
+
+}
+
+/**
+ * @brief Namespace defined to identify data structures that define the parts itself
+ * 
+ */
+namespace pick_part 
+{   
+    /**
+     * @brief A sttructure that stores the part color and type
+     * 
+     */
+    struct Part{
+        uint8_t color;
+        uint8_t type;
+    };
+    /**
+     * @brief Structure to store information about the parts. It stores data like quantity,color,type and bin number in which part needs to go into.
+     * Additionally, it also stores whether the part has to be picked up by ceiling robot or the floor robot in a string.
+     * 
+     */
+    struct PartInfo{
+        uint8_t quantity;
+        std::string pickup_type;
+        uint8_t color;
+        uint8_t type;
+        uint8_t bin_number;
+    };
+    /**
+     * @brief Class that uses the structures that have been defined above to store all data about parts.
+     * 
+     */
+    class Parts {
+        public:
+        std::vector<PartInfo> parts;
+    };
+}
+
 
 /**
  * @brief Class that contains subscriber and callback methods whenever an order or the competitition state is received. 
@@ -116,37 +194,59 @@ class CompetitionARIAC : public rclcpp::Node
      * Also a client is created to send message when an order is submitted
      * 
      */
-    CompetitionARIAC() : Node("competition_subscriber")
-    {
-        m_callback_group_1 = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
-        m_callback_group_2 = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+        CompetitionARIAC() : Node("competition_subscriber")
+        {
+            m_callback_group_1 = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+            m_callback_group_2 = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+            m_callback_group_3 = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+            m_callback_group_4 = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
 
-        //subscription callback groups
-        auto subscription_option1 = rclcpp::SubscriptionOptions();
-        subscription_option1.callback_group = m_callback_group_1;
-        auto subscription_option2 = rclcpp::SubscriptionOptions();
-        subscription_option2.callback_group = m_callback_group_2;
+            //subscription callback groups
+            auto subscription_option1 = rclcpp::SubscriptionOptions();
+            subscription_option1.callback_group = m_callback_group_1;
+            auto subscription_option2 = rclcpp::SubscriptionOptions();
+            subscription_option2.callback_group = m_callback_group_2;
+            auto subscription_option3 = rclcpp::SubscriptionOptions();
+            subscription_option3.callback_group = m_callback_group_3;
+            auto subscription_option4 = rclcpp::SubscriptionOptions();
+            subscription_option4.callback_group = m_callback_group_4;
 
-        // Subscriber objects            
-        comp_state_sub = this->create_subscription<ariac_msgs::msg::CompetitionState>("/ariac/competition_state", 10, 
-                                                                    std::bind(&CompetitionARIAC::CompetitionStateCallback, this, std::placeholders::_1),subscription_option1);
+            // Subscriber objects            
+            comp_state_sub = this->create_subscription<ariac_msgs::msg::CompetitionState>("/ariac/competition_state", 10, 
+                                                                        std::bind(&CompetitionARIAC::CompetitionStateCallback, this, std::placeholders::_1),subscription_option1);
 
-        order_sub = this->create_subscription<ariac_msgs::msg::Order>("/ariac/orders", 10, 
-                                                                    std::bind(&CompetitionARIAC::OrderCallback, this, std::placeholders::_1),subscription_option2);
+            order_sub = this->create_subscription<ariac_msgs::msg::Order>("/ariac/orders", 10, 
+                                                                        std::bind(&CompetitionARIAC::OrderCallback, this, std::placeholders::_1),subscription_option2);
 
-        submit_order_client_ = create_client<ariac_msgs::srv::SubmitOrder>("/ariac/submit_order");
+            bin_part_sub = this->create_subscription<ariac_msgs::msg::BinParts>("/ariac/bin_parts", 10, 
+                                                                        std::bind(&CompetitionARIAC::BinPartCallback, this, std::placeholders::_1),subscription_option3); 
 
-    }
+            conv_part_sub = this->create_subscription<ariac_msgs::msg::ConveyorParts>("/ariac/conveyor_parts", 10, 
+                                                                        std::bind(&CompetitionARIAC::ConvPartCallback, this, std::placeholders::_1),subscription_option4); 
+                   
+            submit_order_client_ = create_client<ariac_msgs::srv::SubmitOrder>("/ariac/submit_order");
+
+        }
+
 
     private:
         
         // flag to check if order is submitted
         bool submitted{false};
-
+        int list_size{0};
+        int count_2{0};
+        int order_index{0};
         bool priority_order = false;
         int priority_index = -1;
         int order_counter{0};
-        int list_size{0};
+
+        bool order_retrived{false};
+        bool bin_flag{false};
+        bool conv_flag{false};
+        std::vector<int> bin_space{1,2,3,4,5,6,7,8};
+        std::vector<int> empty_bins;
+        std::vector<int> order_bins;
+        int slot{1};
 
         /**
          * @brief Vector to store list of orders
@@ -154,17 +254,35 @@ class CompetitionARIAC : public rclcpp::Node
          */
         std::vector<order_::Orders> orders_list;
         std::vector<order_::Orders> orders_list_submit;
+        pick_part::Parts partp;
 
         // Callback Groups
         rclcpp::CallbackGroup::SharedPtr m_callback_group_1;
         rclcpp::CallbackGroup::SharedPtr m_callback_group_2;
+        rclcpp::CallbackGroup::SharedPtr m_callback_group_3;
+        rclcpp::CallbackGroup::SharedPtr m_callback_group_4;
 
         /*==============
         Subscribers
         ==============*/
         rclcpp::Subscription<ariac_msgs::msg::CompetitionState>::SharedPtr comp_state_sub;
         rclcpp::Subscription<ariac_msgs::msg::Order>::SharedPtr order_sub;
+        rclcpp::Subscription<ariac_msgs::msg::BinParts>::SharedPtr bin_part_sub;
+        rclcpp::Subscription<ariac_msgs::msg::ConveyorParts>::SharedPtr conv_part_sub;
 
+
+
+        /**
+         * @brief Function that checks the avaiilable parts in bins and conveyor and prompts the user if the task can be completed succesfully or not.
+         * 
+         */
+        void OrderAccomplishment();
+
+        /**
+         * @brief Function that makes the floor robot pick tray and put it on a AGV
+         * 
+         */
+        void FloorRobot();
 
         /**
          * @brief Function that calls the service to start the competition
@@ -179,6 +297,20 @@ class CompetitionARIAC : public rclcpp::Node
          * @param order_msg 
          */
         void OrderCallback(const ariac_msgs::msg::Order::SharedPtr order_msg); 
+
+         /**
+         * @brief Callback function that gets called when the information about parts in bins are received
+         * 
+         * @param bin_part_msg 
+         */
+        void BinPartCallback(const ariac_msgs::msg::BinParts::SharedPtr bin_part_msg);  
+
+        /**
+         * @brief Callback function that gets called when the information about parts in the conveyor belt are received
+         * 
+         * @param conv_part_msg 
+         */
+        void ConvPartCallback(const ariac_msgs::msg::ConveyorParts::SharedPtr conv_part_msg);
 
         /**
          * @brief Function that calls the service to start the competition
--- src/rwa2.cpp
+++ src/rwa2.cpp
@@ -23,43 +23,45 @@ void CompetitionARIAC::CompetitionStateCallback(const ariac_msgs::msg::Competiti
         RCLCPP_INFO_STREAM(rclcpp::get_logger("CompState Subscriber"), "Competition state is 1, calling service client to Start Competition...");
         callService_start();
     }
-    else if (msg->competition_state == ariac_msgs::msg::CompetitionState::STARTED && orders_list.size() > 0 )
+    else if (msg->competition_state == ariac_msgs::msg::CompetitionState::STARTED && count_2 == 0)
     {
-        // RCLCPP_INFO(rclcpp::get_logger("CompState Subscriber"), "Competition state is 2, Competition Started...\n");    
+        RCLCPP_INFO(rclcpp::get_logger("CompState Subscriber"), "Competition state is 2, Competition Started...\n");
+        count_2++;
     }
     else if (msg->competition_state == ariac_msgs::msg::CompetitionState::ORDER_ANNOUNCEMENTS_DONE && submitted == false)
     {
-          
         RCLCPP_INFO(rclcpp::get_logger("CompState Subscriber"), "Competition state is 3, all orders retrived...\n");
-        RCLCPP_INFO_STREAM(rclcpp::get_logger("Order Submission    "), "Submitting Orders ...");
-        for (int i = 0; i < int(orders_list_submit.size()); i++)
+
+        if(bin_flag && conv_flag && orders_list.size() > 0) 
         {
-            if (orders_list_submit[i].priority == 1)
+            OrderAccomplishment(); 
+        }
+        
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("Order Submission    "), "Submitting Orders ...");
+        for (int i = 0; i < int(orders_list.size()); i++)
+        {
+            if (orders_list[i].priority == 1)
             {
                 RCLCPP_INFO_STREAM(rclcpp::get_logger("Order Submission    "), "Order submitted with Priority 1 :" + orders_list[i].id);
-                callService_submit(orders_list_submit[i].id);
+                callService_submit(orders_list[i].id);
             }
         }
-        for (int i = 0; i < int(orders_list_submit.size()); i++)
+        for (int i = 0; i < int(orders_list.size()); i++)
         {
-            if (orders_list_submit[i].priority == 0)
+            if (orders_list[i].priority == 0)
             {
                 RCLCPP_INFO_STREAM(rclcpp::get_logger("Order Submission    "), "Order submitted with Priority 0 :" + orders_list[i].id);
-                callService_submit(orders_list_submit[i].id);
+                callService_submit(orders_list[i].id);
             }
         }
-        submitted = true;  
-          
+
+        submitted = true;
     }
     else if (msg->competition_state == ariac_msgs::msg::CompetitionState::ORDER_ANNOUNCEMENTS_DONE && submitted == true)
     {
         RCLCPP_INFO(rclcpp::get_logger("CompState Subscriber"), "Competition state is 3, all orders submitted, calling service client to End Competition...");
         callService_end();
         comp_state_sub.reset();
-    }
-    else if (msg->competition_state == ariac_msgs::msg::CompetitionState::ENDED)
-    {
-        RCLCPP_INFO(rclcpp::get_logger("CompState Subscriber"), "Competition state is 4, Competition Ended...");
     }
 }
 
@@ -71,9 +73,7 @@ void CompetitionARIAC::CompetitionStateCallback(const ariac_msgs::msg::Competiti
  */
 void CompetitionARIAC::OrderCallback(const ariac_msgs::msg::Order::SharedPtr order_msg)
 {
-    RCLCPP_INFO(rclcpp::get_logger("Orders    Subscriber"), " ");
     RCLCPP_INFO(rclcpp::get_logger("Orders    Subscriber"), "Received order !");
-    
     order_::Orders order;
     order.id = order_msg->id;
     order.type = order_msg->type;
@@ -128,24 +128,241 @@ void CompetitionARIAC::OrderCallback(const ariac_msgs::msg::Order::SharedPtr ord
             order.AssemComb_type.parts.push_back(ac_part);
         }
     }
-    order_counter++;
-    
-    if(order.priority)
-        priority_index = order_counter - 1;
-    priority_order = order.priority;
+
     order_::Orders order_obj;
 
     RCLCPP_INFO_STREAM(rclcpp::get_logger("Orders    Subscriber"), "  Orders details - ");
     RCLCPP_INFO_STREAM(rclcpp::get_logger("Orders    Subscriber"), "\tOrder ID      : " + order.id);
     RCLCPP_INFO_STREAM(rclcpp::get_logger("Orders    Subscriber"), "\tOrder type    : " + order_obj.order_type[order.type]);
-    RCLCPP_INFO_STREAM(rclcpp::get_logger("Orders    Subscriber"), "\tPriority      : " + std::to_string(order.priority));
-    RCLCPP_INFO_STREAM(rclcpp::get_logger("Orders    Subscriber"), "\torder_counter : " + std::to_string(order_counter)+"\n");
+    RCLCPP_INFO_STREAM(rclcpp::get_logger("Orders    Subscriber"), "\tPriority      : " + std::to_string(order.priority)+"\n");
 
     orders_list.push_back(order);
-    orders_list_submit.push_back(order);
     list_size = orders_list.size();
 
 }
+
+
+/**
+ * @brief Function to check if the necessary parts are available in the bins and conveyor belt and prompts the user if the order can be completed successfully or not.
+ * 
+ */
+void CompetitionARIAC::OrderAccomplishment()
+{
+    color_::ColorParts Col_part_obj;
+    order_::Orders order = orders_list[order_index];
+    std::vector<order_::KittingPart> order_parts = order.kitting_type.parts;
+    order_::KittingType kits ;
+    pick_part::PartInfo bpart;
+    kits.agv_number = order.kitting_type.agv_number;
+    kits.destination = order.kitting_type.destination;
+    RCLCPP_INFO_STREAM(rclcpp::get_logger("Parts availability  "), "Checking for availability of Parts... \n"); 
+    if(order.type == ariac_msgs::msg::Order::KITTING) 
+    {
+        for(int i = 0; i < int(order_parts.size()); i++) {
+            if(order_parts[i].part_status == 0) {
+                for(int j = 0; j < int(partp.parts.size()); j++) {
+                    if(partp.parts[j].quantity != 0) {
+                        if(partp.parts[j].color == order_parts[i].color && partp.parts[j].type == order_parts[i].type) {
+                            
+                            RCLCPP_INFO_STREAM(rclcpp::get_logger("Parts   availability"), "Parts: "); 
+                            RCLCPP_INFO_STREAM(rclcpp::get_logger("Parts   availability"), "  -Type  : " + Col_part_obj.PART[order_parts[i].type]);
+                            RCLCPP_INFO_STREAM(rclcpp::get_logger("Parts   availability"), "  -Color : " + Col_part_obj.COLOR[order_parts[i].color]);
+                            RCLCPP_INFO_STREAM(rclcpp::get_logger("Parts   availability"), "  -Quantity : " + std::to_string(partp.parts[j].quantity));
+                            RCLCPP_INFO_STREAM(rclcpp::get_logger("Parts   availability"), "  -Can be found in Bin " + std::to_string(partp.parts[j].bin_number));
+                            RCLCPP_INFO_STREAM(rclcpp::get_logger("Ceiling  Robot task "), "   Pick " + Col_part_obj.COLOR[order_parts[i].color]+ " "+Col_part_obj.PART[order_parts[i].type]+" from Bin " + std::to_string(partp.parts[j].bin_number));
+                            if(partp.parts[j].quantity == 1)  
+                                RCLCPP_INFO_STREAM(rclcpp::get_logger("Parts availability  "), "   " + Col_part_obj.COLOR[order_parts[i].color]+ " "+Col_part_obj.PART[order_parts[i].type] + " exhausted in Bin " + std::to_string(partp.parts[j].bin_number));
+                            RCLCPP_INFO_STREAM(rclcpp::get_logger("Ceiling  Robot task "), "   Place " + Col_part_obj.COLOR[order_parts[i].color]+ " "+Col_part_obj.PART[order_parts[i].type]+" on AGV "+std::to_string(kits.agv_number)+" at QUADRANT "+std::to_string(order_parts[i].quadrant)+"\n");
+    
+                            partp.parts[j].quantity -= 1;
+                            order_parts[i].part_status = 1;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        bool order_status = 1;
+        for (int i = 0; i < int(order_parts.size()); i++) {
+            if(order_parts[i].part_status == 0) {
+                RCLCPP_INFO_STREAM(rclcpp::get_logger("Parts availability  "), "Part Missing: [" + Col_part_obj.PART[order_parts[i].type] + "," 
+                + Col_part_obj.COLOR[order_parts[i].color] + "]");
+                order_status = 0;
+            }
+        }
+
+        order.order_status = order_status;
+        if(order.order_status == 0)
+            RCLCPP_INFO_STREAM(rclcpp::get_logger("Parts availability  "), "Order ID : " + order.id + " faces Insufficient Parts Challenge!!! \n");
+
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("AGV Destination task"),"\t Lock AGV " + std::to_string(kits.agv_number));
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("AGV Destination task"),"\t Move AGV to " + Col_part_obj.DEST[kits.destination]+"\n");
+
+        if(order_index < int(orders_list.size())) {
+            order_index += 1;
+        }
+
+    }
+}
+
+/**
+ * @brief Callback function that gets called when the subscriber subscribes to the topic ariac/bin_parts topic and stores the details
+ * about the available parts in a data structure
+ * 
+ * @param msg 
+ */
+void CompetitionARIAC::BinPartCallback(const ariac_msgs::msg::BinParts::SharedPtr msg)
+{
+    color_::ColorParts Col_part_obj;
+    RCLCPP_INFO(rclcpp::get_logger("Bin Parts subscriber"), "Parts available in Bins - ");
+
+    //  Loop through all the bin info in the message and store it in the data structure
+    for (const auto &bin_info : msg->bins)
+    {    
+        // Loop through all the part lots in the bin info and store it in the data structure
+        for (const auto &part_lot : bin_info.parts)
+        {
+            
+            pick_part::PartInfo info;
+            info.quantity = part_lot.quantity;
+            info.bin_number = bin_info.bin_number;
+            info.pickup_type = "bin";
+            // Loop through all the parts in the part lot and store it in the data structure
+            std::vector<ariac_msgs::msg::Part> parts_vector;
+            // add the Part object to the container
+            parts_vector.push_back(part_lot.part);
+            // create a new vector from the container
+            auto parts_array = std::vector<ariac_msgs::msg::Part>(parts_vector.begin(), parts_vector.end());
+
+            for (auto &parts : parts_array)
+            {
+                info.color = parts.color;
+                info.type = parts.type;
+            }
+            partp.parts.push_back(info);
+        }
+
+    }
+
+    for (long unsigned int i = 0; i < partp.parts.size(); i++)
+    {
+        if(partp.parts[i].pickup_type == "bin") 
+        {    
+            RCLCPP_INFO_STREAM(rclcpp::get_logger("Bin Parts subscriber"), "\t- Bin Number : " + std::to_string(partp.parts[i].bin_number));
+            RCLCPP_INFO_STREAM(rclcpp::get_logger("Bin Parts subscriber"), "\t  - Parts : ");
+            RCLCPP_INFO_STREAM(rclcpp::get_logger("Bin Parts subscriber"), "\t    - Color  : " + Col_part_obj.COLOR[partp.parts[i].color]);
+            RCLCPP_INFO_STREAM(rclcpp::get_logger("Bin Parts subscriber"), "\t    - Type   : " + Col_part_obj.PART[partp.parts[i].type]);
+            RCLCPP_INFO_STREAM(rclcpp::get_logger("Bin Parts subscriber"), "\t  - Quantity : " + std::to_string(partp.parts[i].quantity));
+        }
+    }
+    bin_flag = true;
+    bin_part_sub.reset();
+}
+
+
+/**
+ * @brief Callback function that gets called when the subscriber subscribes to the topic ariac/conveyor_parts topic and stores the details
+ * about the available parts in a data structure
+ * 
+ * @param conv_part_msg 
+ */
+void CompetitionARIAC::ConvPartCallback(const ariac_msgs::msg::ConveyorParts::SharedPtr conv_part_msg)
+{
+    if (bin_flag)
+    {
+        color_::ColorParts Col_part_obj;
+        RCLCPP_INFO(rclcpp::get_logger("Conveyor Parts subs "), "Parts available on Conveyor Belt - ");
+
+        for (const auto &part_lot : conv_part_msg->parts)
+        {
+            pick_part::PartInfo info;
+            info.quantity = part_lot.quantity;
+            info.pickup_type = "conveyor";
+            info.bin_number = 0;
+            // Loop through all the parts in the part lot and store it in the data structure
+            std::vector<ariac_msgs::msg::Part> parts_vector;
+            parts_vector.push_back(part_lot.part);
+            auto parts_array = std::vector<ariac_msgs::msg::Part>(parts_vector.begin(), parts_vector.end());
+
+            for (auto &parts : parts_array)
+            {
+                info.color = parts.color;
+                info.type = parts.type;
+            }
+            partp.parts.push_back(info);
+        }
+
+        
+        long unsigned int parts_size = partp.parts.size();
+        for (long unsigned int j = 0; j < parts_size; j++)
+        {
+            if(partp.parts[j].pickup_type == "conveyor") 
+            {
+                RCLCPP_INFO_STREAM(rclcpp::get_logger("Conveyor Parts subs "), "\t- Parts : ");
+                RCLCPP_INFO_STREAM(rclcpp::get_logger("Conveyor Parts subs "), "\t    - Color  : " + Col_part_obj.COLOR[partp.parts[j].color]);
+                RCLCPP_INFO_STREAM(rclcpp::get_logger("Conveyor Parts subs "), "\t    - Type   : " + Col_part_obj.PART[partp.parts[j].type]);
+                RCLCPP_INFO_STREAM(rclcpp::get_logger("Conveyor Parts subs "), "\t  - Quantity : " + std::to_string(partp.parts[j].quantity));
+            }
+        }
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("Conveyor Parts subs "), " ");
+
+        for (long unsigned int j = 0; j < parts_size; j++) 
+        {
+            order_bins.push_back(partp.parts[j].bin_number);
+        }
+
+        for(auto bin: bin_space){
+            if(std::find(order_bins.begin(), order_bins.end(), bin) == order_bins.end()){
+                empty_bins.push_back(bin);
+            }
+        } 
+
+        for (long unsigned int j = 0; j < parts_size; j++)
+        {
+            if(partp.parts[j].pickup_type == "conveyor") 
+            {
+                partp.parts[j].bin_number = empty_bins[0];
+                for(int i=0 ; i<partp.parts[j].quantity ; i++)
+                {
+                    RCLCPP_INFO_STREAM(rclcpp::get_logger("Floor    Robot task "), "\t Pick " + Col_part_obj.COLOR[partp.parts[j].color] + " " + Col_part_obj.PART[partp.parts[j].type] + " and place in Bin " + std::to_string(partp.parts[j].bin_number) +" - slot " + std::to_string(slot));
+                    slot++;
+                }               
+            }
+        }
+
+        FloorRobot();
+
+        conv_flag = true;
+        conv_part_sub.reset();
+    }
+}
+
+
+/**
+ * @brief Function that makes the floor robot pick tray and put it on a AGV
+ * 
+ */
+
+void CompetitionARIAC::FloorRobot()
+{
+    color_::ColorParts Col_part_obj;
+    order_::Orders order = orders_list[order_index];
+    order_::KittingType kits ;
+
+    kits.agv_number = order.kitting_type.agv_number;
+    kits.tray_id = order.kitting_type.tray_id;
+
+    if(order.type == ariac_msgs::msg::Order::KITTING) 
+    {    
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("Floor    Robot task "),"");
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("Floor    Robot task "),"\t Change to tray gripper ");
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("Floor    Robot task "),"\t Pick Tray number " + std::to_string(kits.tray_id));
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("Floor    Robot task "),"\t Place tray on AGV " + std::to_string(kits.agv_number));
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("Floor    Robot task "),"\t Change to part gripper\n");
+    }
+}
+
+
 
 /**
  * @brief Function that calls the trigger service to start the competition depending on competition state
