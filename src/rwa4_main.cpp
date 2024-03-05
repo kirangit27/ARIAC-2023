7a7503c090d8f1f8d081067d4f33c1eeee8a56c4
Kiran S Patil <kiran.suvas.patil@gmail.com>
2024-03-05 18:03:01 -0500

    updated main file for rwa4

--- src/rwa4_main.cpp
+++ src/rwa4_main.cpp
@@ -1,16 +1,15 @@
 /**
- * @file rwa2_main.cpp
+ * @file rwa4_main.cpp
  * @author Kiran S Patil,Aniruddh Balram,Vyshnv Achuthan,Badrinarayanan
- * @brief Main function for rwa1.cpp. A Multithreded executor is also defined to perform multithreading 
+ * @brief Main function for rwa1.cpp.A Multithreded executor is also defined to perform multithreading 
  * to avoid deadlocks
  * @version 0.1
- * @date 2023-03-17
+ * @date 2023-04-30
  * 
  * @copyright Copyright (c) 2023
  * 
  */
-
-#include "../include/ARIAC-2023/rwa3.hpp"
+#include "../include/ARIAC-2023/rwa4.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 int main(int argc, char **argv)
